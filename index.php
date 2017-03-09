ad2d44f8fa9849f2ab2f930c7baaf7bda26651b4
10-11 <10-11>
2017-03-09 10:27:45 -0300

    formulário pronto

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
-        inicio da página
+        inicio da página 
+        <form>
+            Nome:<input type="text" name="nome" placeholder="coloque seu nome"><br>
+            Sobrenome:<input type="text" name="sobrenome" placeholder="coloque seu sobrenome"><br>
+            senha:<input type="password" name="senha" placeholder="coloque sua senha"><br>
+            <input type="button" name="Enviar" value="enviar">
+        </form>
     </body>
 </html>
